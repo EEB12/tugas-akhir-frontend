4584eef53ede5e235546e51d8b635aa1422aaea8
EEB12 <rayhan.taqara@yahoo.com>
2023-07-12 04:08:36 +0700

    fix tampilan

--- src/pages/list_penelitian/ListPenelitian.jsx
+++ src/pages/list_penelitian/ListPenelitian.jsx
@@ -62,7 +62,7 @@ const Listpenelitian = () => {
         const response = await axios({
             method: "get",
             url: `https://backend-ta.ndne.id/api/get_detail_penelitian/${id}}`,
-           
+
             headers: {
                 "Authorization": `Bearer ${token}`,
                 'Content-Type': 'application/json'
@@ -70,7 +70,7 @@ const Listpenelitian = () => {
         }).then(data => data);
 
         console.log(response.data)
-        const dataDownload=response.data[1]
+        const dataDownload = response.data[1]
         var headers = Object.keys(dataDownload[0])
         console.log(headers)
         const dataToConvert = {
@@ -93,7 +93,7 @@ const Listpenelitian = () => {
             if (role == '"peneliti"' || role == '"anotator"') {
                 const response = await axios({
                     method: "get",
-                    url: "https://backend-ta.ndne.id/api/list_penelitian",
+                    url: "https://backend-ta.ndne.id/api/my_list_penelitian",
 
                     headers: {
                         "Authorization": `Bearer ${token}`,
@@ -157,13 +157,13 @@ const Listpenelitian = () => {
                             alignItems: 'center'
                         }}>
 
-                            <Grid container spacing={1}>
+                            <Grid container >
                                 {/* Chart */}
                                 <Grid item xs={12} md={12} lg={12}>
                                     <Paper elevation={0}
-                                        
+
                                         sx={{
-                                            width: '80%',
+                                            width: '100%',
                                             height: '100%',
                                             overflowX: 'hidden',
                                             position: 'fixed',
@@ -172,16 +172,155 @@ const Listpenelitian = () => {
                                         }}
 
                                     >
-                                        <div className='container-fluid'>
-                                            <div className='row mb-5'>
+                                        <div className='container   '>
 
 
-                                                <Typography sx={{
-                                                    color: '#0285F1',
-                                                    fontWeight: 600, m: 1, fontSize: 60
-                                                }} variant="h3" gutterBottom>
-                                                    List Penelitian Finished
-                                                </Typography>
+                                            <div className='row  d-flex justify-content-around'>
+
+                                                <div id="carouselExampleCaptions" class="carousel slide  " data-bs-ride="carousel">
+                                                    <div class="carousel-indicators">
+                                                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                                    </div>
+                                                    <div class="carousel-inner rounded-5">
+
+                                                        <div class="carousel-item active">
+
+                                                            <img src="/carousel-2.jpg" class="d-block" style={{ width: '100%', height: '467px' }} alt="..." />
+                                                            <div class="carousel-caption custom-first px-5 d-none d-md-block rounded-4" >
+                                                                <h1>Selamat Datang di Website Anotasi</h1>
+                                                                <h5 className='fw-normal'>Website Anotasi Data merupakan website yang menyediakan fitur anotasi data manual maupun otomatis
+                                                                    untuk mahasiswa dalam melakukan penelitian anotasi data. Website ini mendukung penggunaan model dalam berbagai penelitian.
+
+                                                                </h5>
+                                                                <p className='mt-3 text-md-center'>
+                                                                    Fitur yang disediakan oleh website ini berupa:
+                                                                    <div class=" gap-3 d-flex align-items-center justify-content-center flex-column mt-3">
+                                                                        <div class=" list-group-mine py-2 fw-normal w-25" href="/#">Membuat Penelitian Data</div>
+                                                                        <div class=" list-group-mine py-2 fw-normal w-25" href="/#">Melakukan Anotasi Data</div>
+                                                                        <div class=" list-group-mine py-2 fw-normal w-25" href="/#">Export Data Hasil Anotasi</div>
+                                                                    </div>
+                                                                  
+                                                                </p>
+
+
+
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="carousel-item">
+                                                            <img src="/carousel.jpg" class="d-block" style={{ width: '100%', maxWidth: '2560px', maxHeight: '467px' }} alt="..." />
+                                                            <div class="carousel-caption custom-second  px-5 d-none d-md-block rounded-4" >
+                                                                <h1 className='text-dark'>Langkah Membuat Penelitian</h1>
+
+                                                                <p className='mt-3 text-dark text-md-center'>
+
+                                                                    <div class=" gap-3 d-flex align-items-center justify-content-center flex-row mt-3">
+                                                                        <div class="  py-2 fw-normal w-25 text-dark" href="/#">
+                                                                            <h4 className='text-center'> Langkah Pertama</h4>
+                                                                            <img style={{ width: '100%', maxWidth: '50px' }} src="/click.png" />
+
+                                                                            <p className='text-center'>
+                                                                                Klik "Buat Penelitian"
+                                                                            </p>
+
+                                                                        </div>
+
+                                                                        <img style={{ width: '100%', maxWidth: '50px' }} src="/arrow.png" />
+
+
+                                                                        <div class="  py-2 fw-normal w-25 text-dark" href="/#">
+                                                                            <h4 className='text-center'> Langkah Kedua</h4>
+                                                                            <img style={{ width: '100%', maxWidth: '50px' }} src="/form.png" />
+                                                                            <p className='text-center'>
+                                                                                Lengkapi Data Penelitian
+                                                                            </p>
+
+
+                                                                        </div>
+
+                                                                        <img style={{ width: '100%', maxWidth: '50px' }} src="/arrow.png" />
+
+                                                                        <div class="  py-2 fw-normal w-25 text-dark" href="/#">
+                                                                            <h4 className='text-center'> Langkah Ketiga</h4>
+                                                                            <img style={{ width: '100%', maxWidth: '50px' }} src="/anotator.png" />
+                                                                            <p className='text-center'>
+                                                                                Pilih Anotator
+                                                                            </p>
+
+                                                                        </div>
+
+
+
+                                                                    </div>
+
+                                                                    <div class=" gap-3 mt-3 d-flex align-items-center justify-content-center flex-row mt-3">
+                                                                        <div class="  py-2 fw-normal w-25 text-dark" href="/#">
+                                                                            <h4 className='text-center'> Langkah Keempat</h4>
+                                                                            <img style={{ width: '100%', maxWidth: '50px' }} src="/time.png" />
+
+                                                                            <p className='text-center'>
+                                                                                Menunggu Proses Anotasi
+                                                                            </p>
+
+                                                                        </div>
+
+                                                                        <img style={{ width: '100%', maxWidth: '50px' }} src="/arrow.png" />
+
+                                                                        <div class="  py-2 fw-normal w-25 text-dark" href="/#">
+                                                                            <h4 className='text-center'> Langkah Kelima</h4>
+                                                                            <img style={{ width: '100%', maxWidth: '50px' }} src="/form.png" />
+                                                                            <p className='text-center'>
+                                                                                Mendapatkan Hasil Anotasi
+                                                                            </p>
+
+
+                                                                        </div>
+                                                                       
+
+
+
+                                                                    </div>
+
+                                                                </p>
+
+
+
+                                                            </div>
+                                                        </div>
+                                                        {/* <div class="carousel-item">
+                                                            <img src="/carousel-2.jpg" class="d-block" style={{ width: '100%', height: '467px' }} alt="..." />
+                                                            <div class="carousel-caption d-none d-md-block">
+                                                                <h5>Third slide label</h5>
+                                                                <p>Some representative placeholder content for the third slide.</p>
+                                                            </div>
+                                                        </div> */}
+                                                    </div>
+                                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                        <span class="visually-hidden">Previous</span>
+                                                    </button>
+                                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                        <span class="visually-hidden">Next</span>
+                                                    </button>
+                                                </div>
+
+
+                                            </div>
+
+                                            <div className='row  mb-4 d-flex justify-content-around'>
+
+                                                <div className='title mx-5 mt-3'>
+                                                    <Typography sx={{
+                                                        color: '#0285F1',
+                                                        fontWeight: 600, m: 1, fontSize: 30
+                                                    }} variant="h3" gutterBottom>
+                                                        List Penelitian Finished
+                                                    </Typography>
+                                                </div>
+
 
                                             </div>
                                             <div className='row'>
