785a9cd5b51bad08daeb6866e884090f6b8a8878
EEB12 <rayhan.taqara@yahoo.com>
2023-07-13 03:25:12 +0700

    fix new penelitian

--- src/pages/new_penelitian/NewPenelitian.jsx
+++ src/pages/new_penelitian/NewPenelitian.jsx
@@ -47,7 +47,9 @@ const NewPenelitian = () => {
   const [desc, setDesc] = useState("");
   const [target, setTarget] = useState("");
   const [inputValue, setInputValue] = useState("");
-
+  const [dragActive, setDragActive] = React.useState(false);
+  // ref
+  const inputRef = React.useRef(null);
   const handleClose = () => {
     setOpen(false);
   };
@@ -84,6 +86,9 @@ const NewPenelitian = () => {
   const handletarget = (event) => {
     setInputValue(event.target.value);
   };
+
+
+
   const handleTargetButton = () => {
 
     // if(inputValue=""){
@@ -109,7 +114,38 @@ const NewPenelitian = () => {
     return Object.keys(data[0]).reverse();
   };
 
+  const handleDrag = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
 
+  // triggers when file is dropped
+  const handleDrop = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      // handleFiles(e.dataTransfer.files);
+    }
+  };
+
+  // triggers when file is selected with click
+  const handleChanged = function (e) {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      // handleFiles(e.target.files);
+    }
+  };
+
+  // triggers the input when the button is clicked
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
   const uploadFiles = async () => {
     // Create a form and post it to server
     handleToggle()
@@ -249,7 +285,7 @@ const NewPenelitian = () => {
                                     <Typography
                                       sx={{
                                         fontWeight: 600,
-                                        
+
                                         fontSize: 35,
                                       }}
                                       variant="h3"
@@ -306,67 +342,67 @@ const NewPenelitian = () => {
                                   </div>
 
                                   <div>
-                                    
-                                      <Typography
-                                        sx={{
-                                          fontWeight: 600,
 
-                                          fontSize: 35,
-                                        }}
-                                        variant="h3"
-                                        gutterBottom
-                                      >
-                                        Target Label
-                                      </Typography>
-                                      <TextField disabled
-                                        id="filled-multiline-static"
-                                        multiline
-                                        value={target}
-                                        variant="filled"
+                                    <Typography
+                                      sx={{
+                                        fontWeight: 600,
 
-                                        sx={{
+                                        fontSize: 35,
+                                      }}
+                                      variant="h3"
+                                      gutterBottom
+                                    >
+                                      Target Label
+                                    </Typography>
+                                    <TextField disabled
+                                      id="filled-multiline-static"
+                                      multiline
+                                      value={target}
+                                      variant="filled"
 
-                                          width: "35%",
-                                          marginBottom: 4,
-                                          backgroundColor: "#FFFFFF",
-                                        }}
-                                      />
+                                      sx={{
 
-                                    
+                                        width: "35%",
+                                        marginBottom: 4,
+                                        backgroundColor: "#FFFFFF",
+                                      }}
+                                    />
+
+
                                   </div>
 
 
                                   <div>
-                                    
-                                      <TextField
-                                        id="filled-multiline-static"
-                                        multiline
-                                        value={inputValue}
-                                        onChange={handletarget}
-                                        sx={{
 
-                                          width: "35%",
-                                          marginBottom: 4,
-                                          backgroundColor: "#FFFFFF",
-                                        }}
-                                      />
+                                    <TextField
+                                      id="filled-multiline-static"
+                                      multiline
+                                      value={inputValue}
+                                      onChange={handletarget}
+                                      sx={{
 
-                                      <Button
-                                        sx={{
+                                        width: "35%",
+                                        marginBottom: 4,
+                                        backgroundColor: "#FFFFFF",
+                                      }}
+                                    />
 
-                                          width: 100,
-                                          height: 50,
+                                    <Button
+                                      sx={{
+
+                                        width: 100,
+                                        height: 50,
 
 
-                                        }}
-                                        type="button"
-                                        variant="contained"
-                                        onClick={handleTargetButton}
-                                        className="ms-2"
-                                      >
-                                        Add
-                                      </Button>
-                                    
+                                      }}
+                                      type="button"
+                                      variant="contained"
+                                      onClick={handleTargetButton}
+                                      className="ms-2"
+                                    >
+                                      Add
+                                    </Button>
+
                                   </div>
 
 
@@ -433,7 +469,7 @@ const NewPenelitian = () => {
 
                                 <div className="col-6">
 
-                                  <Box
+                                  {/* <Box
                                     sx={{
                                       marginLeft: "8px",
                                       marginTop: 1,
@@ -454,7 +490,18 @@ const NewPenelitian = () => {
                                       type="file"
                                       onChange={handleFilesChange}
                                     />
-                                  </Box>
+                                  </Box> */}
+
+                                  <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+                                    <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChanged} />
+                                    <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : ""}>
+                                      <div>
+                                        <p>Drag and drop your file here or</p>
+                                        <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
+                                      </div>
+                                    </label>
+                                    {dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div>}
+                                  </form>
                                 </div>
 
 
